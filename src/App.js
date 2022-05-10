6de67cf36decf6d2ecbab3f30a460a5e8da6188c
argiriou <argiriou.a@gmail.com>
2022-05-11 01:17:23 +0300

    Show Calendar in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Calendar from './Calendar';
 import './styles.css';
 import './colors.css';
 
 export default function App() {
   return (
     <div className="App">
+      <Calendar />
     </div>
   );
 }
