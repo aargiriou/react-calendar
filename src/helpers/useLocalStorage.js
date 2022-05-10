327e8b382a81ae48bdbdd1191eb3e49b8b5e1169
argiriou <argiriou.a@gmail.com>
2022-05-11 01:13:37 +0300

    Create helper hook useLocalStorage()

---
+++ src/helpers/useLocalStorage.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export default function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+      
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+
+      return initialValue;
+    }
+  });
+  
+  const setValue = (value) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      
+      setStoredValue(valueToStore);
+      
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+}
